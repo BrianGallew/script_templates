68beafcf1144738cdc0f6ad7dcae2c7d6e9a6e86
Brian Gallew <brian.gallew@crowdstrike.com>
2018-07-26 12:48:36 -0700

    Fix names to not annoy pylint anymore, re-use values for debugging

--- template.py
+++ template.py
@@ -9,39 +9,38 @@ https://github.com/BrianGallew/script_templates/blob/master/template.py
 import logging
 import logging.handlers
 import argparse
-logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
-option_group = argparse.Namespace()  # pylint: disable=invalid-name
+LOGGER = logging.getLogger(__name__)
+OPTION_GROUP = argparse.Namespace()
 
 
 def setup_logging():
     """Sets up logging in a syslog format by log level
-    :param option_group: options as returned by the OptionParser
+    :param OPTION_GROUP: options as returned by the OptionParser
     """
     stderr_log_format = "%(levelname) -8s %(asctime)s %(funcName)s line:%(lineno)d: %(message)s"
     file_log_format = "%(asctime)s - %(levelname)s - %(message)s"
-    if option_group.debug:
-        logger.setLevel(level=logging.DEBUG)
-    elif option_group.verbose:
-        logger.setLevel(level=logging.INFO)
-    else:
-        logger.setLevel(level=logging.WARNING)
+    syslog_format = "%(processName)s[%(process)d] - %(message)s"
+    LOGGER.setLevel(level=OPTION_GROUP.loglevel)
 
     handlers = []
-    if option_group.syslog:
+    if OPTION_GROUP.syslog:
         handlers.append(
-            logging.handlers.SysLogHandler(facility=option_group.syslog))
+            logging.handlers.SysLogHandler(facility=OPTION_GROUP.syslog, address='/dev/log'))
+        handlers[-1].setFormatter(logging.Formatter(syslog_format))
         # Use standard format here because timestamp and level will be added by
         # syslogd.
-    if option_group.logfile:
-        handlers.append(logging.FileHandler(option_group.logfile))
+    if OPTION_GROUP.logfile:
+        handlers.append(logging.FileHandler(OPTION_GROUP.logfile))
         handlers[0].setFormatter(logging.Formatter(file_log_format))
     if not handlers:
         handlers.append(logging.StreamHandler())
         handlers[0].setFormatter(logging.Formatter(stderr_log_format))
+    # Remove all the old handler(s)
     for handler in logging.root.handlers:
         logging.root.removeHandler(handler)
+    # Add our new handler(s) back in
     for handler in handlers:
-        logger.root.addHandler(handler)
+        LOGGER.root.addHandler(handler)
     return
 
 
@@ -50,17 +49,19 @@ def main():
     parser = argparse.ArgumentParser(description=__doc__,
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     # Standard logging options.
-    parser.add_argument("-v", "--verbose", action='store_true',
-                        default=False, help="Verbose output")
-    parser.add_argument("-d", "--debug", action='store_true',
-                        default=False, help="Debugging output")
+    parser.add_argument("-v", "--verbose", action='store_const',
+                        const=logging.INFO, dest='loglevel',
+                        default=logging.WARNING, help="Verbose output")
+    parser.add_argument("-d", "--debug", action='store_const',
+                        const=logging.DEBUG, dest='loglevel',
+                        default=logging.WARNING, help="Debugging output")
     parser.add_argument("--syslog", metavar="FACILITY",
                         help="Send log messages to the syslog")
     parser.add_argument("--logfile", metavar="FILENAME",
                         help="Send log messages to a file")
     # script-specific options here
 
-    parser.parse_args(namespace=option_group)
+    parser.parse_args(namespace=OPTION_GROUP)
     setup_logging()
 
     # Your code here.
